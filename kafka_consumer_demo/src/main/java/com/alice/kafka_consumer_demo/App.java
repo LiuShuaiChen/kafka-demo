28019b514e0fb24e980048eb2184fbec2c205992
alice <alice@alice-PC>
2018-11-13 17:05:36 +0800

    kafka_consumer_demo 提交

---
+++ kafka_consumer_demo/src/main/java/com/alice/kafka_consumer_demo/App.java
@@ -0,0 +1,13 @@
+package com.alice.kafka_consumer_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
